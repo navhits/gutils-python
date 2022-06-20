1ff6c45cbc4373aa57a78c166bf81c8ee89030ae
Naveen S <dev@navs.page>
2022-06-20 18:11:13 +0530

    Adding 2 additional variables

--- gutils/creds/__init__.py
+++ gutils/creds/__init__.py
@@ -9,7 +9,7 @@ from typing import Optional
 def Secret(environment_variable: str,
         required: bool, default_value: Optional[str] = None) -> None:
     """
-    This class checks and imports the environment variables and raises exception if they are not set.
+    This function checks and imports the environment variables and raises exception if they are not set.
     """
     value = os.getenv(environment_variable)
     if not value and default_value:
@@ -22,6 +22,8 @@ def Secret(environment_variable: str,
 OAUTH_CREDS_DIR = "gutils/creds/google/oauth"
 GCP_OAUTH_CLIENT_ID = Secret(environment_variable="GCP_OAUTH_CLIENT_ID", required=False)
 GCP_OAUTH_CLIENT_SECRET = Secret(environment_variable="GCP_OAUTH_CLIENT_SECRET", required=False)
+GCP_OAUTH_AUTH_TOKEN = Secret(environment_variable="GCP_OAUTH_AUTH_TOKEN", required=False)
+GCP_OAUTH_REFRESH_TOKEN = Secret(environment_variable="GCP_OAUTH_REFRESH_TOKEN", required=False)
 GCP_PROJECT_ID = Secret(environment_variable="GCP_PROJECT_ID", required=False)
 
 GCP_SERVICE_ACCOUNT_PRIVATE_KEY_ID = Secret(environment_variable="GCP_SERVICE_ACCOUNT_PRIVATE_KEY_ID", required=False)
